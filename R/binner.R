3d47b87d743bb901e9c7bad458e4506fe4d1a89e
Alexander Matrunich <alexander@matrunich.com>
2017-09-08 12:59:36 +0400

    Add binner()

---
+++ R/binner.R
@@ -0,0 +1,22 @@
+#' Simple function with Shiny app inside
+#'
+#' @import shiny
+#' @export
+
+binner <- function() {
+  shinyApp(
+    ui = fluidPage(
+      sidebarLayout(
+        sidebarPanel(sliderInput("n", "Количество групп", 5, 100, 20)),
+        mainPanel(plotOutput("hist"))
+      )
+    ),
+    server = function(input, output) {
+      output$hist <- renderPlot(
+        hist(faithful$eruptions, breaks = input$n,
+             col = "skyblue", border = "white",
+             main = "Продолжительность извержений гейзера, м")
+      )
+    }
+  )
+}
