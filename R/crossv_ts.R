5b80e4a9698038be5297e4158732d4520f5740af
Alexander Matrunich <alexander@matrunich.com>
2017-11-02 12:32:33 +0400

    crossv_ts() returns time series with errors

--- R/crossv_ts.R
+++ R/crossv_ts.R
@@ -1,15 +1,17 @@
 #' Time series cross-validation
 #'
 #' Computes the forecast errors obtained by applying forecastfunction to subsets
-#' of the time series y using a rolling forecast origin. It differs from
-#' forecast::tsCV by skipping head of the time series, when it is too shot.
+#' of the time series y using a rolling (accumulating) forecast origin. It
+#' differs from forecast::tsCV by skipping head of the time series.
 #'
 #' @param y Time series object.
 #' @param forecastfunction Forecasting function. It must be able take ts object as first parameter and h (forecasting horizon) as second parameter.
+#' @param h Forecast horizon.
 #' @param tail2CV Integer. It specifies number of time series elements for those predicitions will be build.
 #' @param ... Additional parameters to forecastfunction.
 #'
-#' @return List of forecast objects.
+#' @return A time series object with time index identical to y and containing
+#'   absolute prediction errors.
 #'
 #' @import forecast
 #' @import purrr
@@ -21,6 +23,8 @@ crossv_ts <- function(y, forecastfunction, h = 1, tail2CV = NULL, ...) {
   stopifnot(is_function(forecastfunction))
   stopifnot(is.ts(y))
   n <- length(y)
+  # If no tail2CV than simply skip first sample
+  # as it is not possible to build a forecast for it
   if (is.null(tail2CV)) tail2CV <- n - 1
   stopifnot(is.numeric(tail2CV))
 
@@ -32,29 +36,23 @@ crossv_ts <- function(y, forecastfunction, h = 1, tail2CV = NULL, ...) {
       ts_short <- subset(ts, end = tsend)
       forecast_fit <- forecastfunction(ts_short, h = h, ...)
       assert_that("forecast" %in% class(forecast_fit))
-      forecast_fit
       # Calculate error
-      # ts[tsend + h] - fc$mean[h]
-    }, NULL)
+      ts[tsend + h] - forecast_fit$mean[h]
+    }, NA_real_)
 
-  # head_skip_n <- n - tail2CV
+  head_skip_n <- n - tail2CV
 
-  map(seq.int(from = n - tail2CV - h + 1, to = n - h),
+  e <- map_dbl(seq.int(from = n - tail2CV, to = n - h),
                ~ possible_frcst(y, h = h, tsend = .x)
   )
 
-  # Produce vector of forecast errors
-  # e <- map_dbl(seq.int(from = n - tail2CV - h + 1, to = n - h),
-  #              ~ possible_frcst_err(y, h = h, tsend = .x)
-  # )
-
   # Add first missing elements
 
-  # e <- c(rep(NA_real_, times = head_skip_n), e)
+  e <- c(rep(NA_real_, times = head_skip_n + h - 1), e)
 
-  # What was it? Convert vector to time series object?
-
-  # y / y * e
+  # Convert vector to time series object with time index identical to
+  # the original time series
+  y / y * e
 
 
 }
--- tests/testthat/test-crossv_ts.R
+++ tests/testthat/test-crossv_ts.R
@@ -1,16 +1,27 @@
 library(forecast)
 data("gas", package = "forecast")
 gas24 <- subset(gas, end = 24L)
+gas6 <- subset(gas, end = 6L)
 ets_frcst <- function(x, h) forecast(ets(x), h = h)
+bats_frcst <- function(x, h) forecast(bats(x), h = h)
 
 context("TS cross validation")
 
-test_that("crossv_ts returns a list", {
-  expect_is(crossv_ts(gas24, ets, tail2CV = 2),
-               "list")
+test_that("crossv_ts returns a ts", {
+  expect_is(crossv_ts(gas24, ets_frcst, tail2CV = 2),
+               "ts")
 })
 
-test_that("crossv_ts returns a list of proper length", {
-  expect_length(crossv_ts(gas24, ets, h = 1, tail2CV = 2),
-               2)
+test_that("crossv_ts returns a ts of proper length", {
+  expect_length(crossv_ts(gas24, ets_frcst, h = 1, tail2CV = 2),
+               24)
+  expect_length(crossv_ts(gas24, ets_frcst, h = 2, tail2CV = 2),
+                24)
+})
+
+test_that("crossv_ts returns a ts with proper number of NA values", {
+  expect_equal(sum(is.na(crossv_ts(gas6, ets_frcst, h = 1, tail2CV = 2))),
+               4)
+  expect_equal(sum(is.na(crossv_ts(gas6, ets_frcst, h = 2, tail2CV = 2))),
+               5)
 })
