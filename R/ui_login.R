37ea638fa6a7b5460572cff5fd42fdc611b4a3a6
Alexander Matrunich <alexander@matrunich.com>
2017-12-05 14:02:09 +0400

    Prefill test name and password

--- R/ui_login.R
+++ R/ui_login.R
@@ -7,8 +7,8 @@
 ui_login <- function() {
   fluidRow(column(width=4, offset = 4,
                   wellPanel(id = "login",
-                            textInput(".username", "Username:"),
-                            passwordInput(".password", "Password:"),
+                            textInput(".username", "Username (test):", "test"),
+                            passwordInput(".password", "Password (123):", "123"),
                             div(actionButton(".login", "Log in"), style="text-align: center;")
                   ),
                   textOutput("message")
